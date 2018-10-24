bfa8246b00e7fa4614fcf657ab85e88bb5ce1545
JavieRodriguez <javier890302@gmail.com>
2018-10-24 08:19:10 -0500

    Update Modalidad.java
    Not Null Import

--- src/main/java/com/example/demo/model/Modalidad.java
+++ src/main/java/com/example/demo/model/Modalidad.java
@@ -10,7 +10,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.NotNull;
+import javax.validation.constraints.NotNull;
 
 /**
  *
