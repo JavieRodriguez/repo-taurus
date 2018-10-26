e4a756ee68e881ba848a9dc08118040e76807866
SergioEstebanPi <sepi_147@hotmail.com>
2018-10-25 20:20:33 -0500

    Agrego configuracion CORS para toda la app

--- src/main/java/com/example/demo/ContratacionApplication.java
+++ src/main/java/com/example/demo/ContratacionApplication.java
@@ -10,6 +10,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class ContratacionApplication {
@@ -19,6 +22,16 @@ public class ContratacionApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ContratacionApplication.class, args);
+    }
+    
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
     }
 
     @Bean
